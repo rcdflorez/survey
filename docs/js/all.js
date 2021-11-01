1e6d946316ad548f962bd10eac0ab935cddbcd9a
Ricardo Florez <system@leapcredit.com>
2021-11-01 18:00:27 -0400

    implement counter and stripped colors

--- docs/js/all.js
+++ docs/js/all.js
@@ -10,28 +10,37 @@
 let resultObj = {};
 let question = "";
 let response = "";
+let responseCounter = 0;
 
 const getSurveyResult = () => {
-  $("div.survey-container li").each(function (index) {
-    question = $(this).text();
+    $("div.survey-container li").each(function(index) {
+        question = $(this).text();
 
-    response = $(this)
-      .parent()
-      .parent()
-      .children("div.col-7")
-      .children("div")
-      .children("div")
-      .children("input:checked")
-      .val();
+        response = $(this)
+            .parent()
+            .parent()
+            .children("div.col-7")
+            .children("div")
+            .children("div")
+            .children("input:checked")
+            .val();
 
-    resultObj[question] = response;
-  });
+        resultObj[question] = response;
+    });
 
-  console.log(resultObj);
+    console.log(resultObj);
 };
 
-$("div.survey-container.mobile :radio").change(function () {
-  $(this).parent().parent().parent().parent().next()[0].scrollIntoView({
-    behavior: "smooth",
-  });
+$("div.survey-container.mobile :radio").change(function() {
+    $(this).parent().parent().parent().parent().next()[0].scrollIntoView({
+        behavior: "smooth",
+    });
+});
+
+$("div.survey-container.desktop :radio").change(function() {
+    responseCounter++;
+
+    if (responseCounter >= 10) {
+        $("a.btn").removeClass("disabled");
+    }
 });
